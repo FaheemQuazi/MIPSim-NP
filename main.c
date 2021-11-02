5ae1d5e424492d6a44db19d15f37a88f43afff43
FaheemQuazi <FaheemQuazi@users.noreply.github.com>
2021-11-02 16:44:30 -0500

    Finish basic pipeline - Add output file code - Add switch statements for different instructions
    Co-authored-by: Stanley, Arien E <aestanle@CougarNet.UH.EDU>
    Co-authored-by: Gtorres0620 <Gtorres0620@users.noreply.github.com>

--- main.c
+++ main.c
@@ -14,28 +14,28 @@ typedef struct  {
 
 
 typedef struct {
-    unsigned int imm:16;
+    int imm:16;
     unsigned int regt:5;
     unsigned int regs:5;
     unsigned int opcode:6;
 } instr_type_I;
 
+typedef struct {
+    int imm:26;
+    unsigned int opcode:6;
+} instr_type_J;
 
 union {
     unsigned int raw;
     instr_type_R R;
     instr_type_I I;
+    instr_type_J J;
 } instr;
-
-// struct instr_type_J {
-//     unsigned int opcode:6;
-//     unsigned int offset:26;
-// } itj;
 
 // CPU components
 int REGISTERS[32];
-int PC;
 int MEMORY[250];
+int PC = 0;
 unsigned int CODE[1024];
 int CODE_LENGTH = 0;
 
@@ -88,52 +88,156 @@ void file_load(char* input_file) {
     fclose(fin);
 }
 
+void file_dumpRegMem(FILE* fout) {
+    // assume fout is already opened
+    int i;
 
+    // Print register info to output file
+    fprintf(fout, "\nREGISTERS\n");
+    for(i = 0; i < 32; ++i){
+        if(REGISTERS[i] != 0){
+            fprintf(fout, "R%d %d\n", i, REGISTERS[i]);
+        }
+    }
 
-void cpu( /* char* output_file */ ) {
+    // Print memory info to output file
+    fprintf(fout, "\nMEMORY\n");
+    for(i = 0; i < 250; ++i){
+        if(MEMORY[i] != 0){
+            fprintf(fout, "%d %d\n", i*4, MEMORY[i]);
+        }
+    }
+}
+
+void cpu(char* output_file) {
+    FILE *fout = fopen(output_file, "w");
+    
+    int clock = 1;   
+    int ic = 1;
+    bool rwMem = false;
+    bool wbReg = false;
 
     while (PC < CODE_LENGTH) {
+        rwMem = false;
+        wbReg = false;
 
-        // IF
-        unsigned int CodeVal = CODE[PC];
-        char InstructType = '0';
+        // IF (and increment PC)
+        fprintf(fout, "C#%d I%d-IF\n", clock++, ic);
+        unsigned int CodeVal = CODE[PC++];
         
         // ID (indentify opcodes: 8 possible) 0 R, 2 or 3 is J, and then everything else is I
+        fprintf(fout, "C#%d I%d-ID\n", clock++, ic);
         int opcode = (CodeVal >> 26);
-        switch(opcode){
-            case 0: 
+        char InstructType = '0';
+        switch(opcode)
+        {
+            case 0: //add, sub, slt
                 InstructType = 'R';
                 instr.raw = CodeVal;
-                printf("Opcode: %u\n", instr.R.opcode);
                 break;
 
             case 2 || 3: 
                 InstructType = 'J';
                 break;
 
-            default: 
+            default: //storeword, loadword, addi, beq, bne
                 InstructType = 'I';
                 instr.raw = CodeVal;
-                printf("Opcode: %u\n", instr.I.opcode);
                 break;
             
         } 
         
-        // RF
-
         // ALU / EX
+        fprintf(fout, "C#%d I%d-EX\n", clock++, ic);
+        switch (InstructType)
+        {
+            case 'R':
+                switch (instr.R.funct)
+                {
+                    case 0b100000: //ADD
+                        REGISTERS[instr.R.regd] = REGISTERS[instr.R.regs] + REGISTERS[instr.R.regt];
+                        wbReg = true;
+                        break;
 
-        // MEM
+                    case 0b100010: //SUB
+                        REGISTERS[instr.R.regd] = REGISTERS[instr.R.regs] - REGISTERS[instr.R.regt];
+                        wbReg = true;
+                        break;               
 
-        // WB
+                    case 0b101010: //SLT 
+                        REGISTERS[instr.R.regd] = REGISTERS[instr.R.regs] < REGISTERS[instr.R.regt];
+                        wbReg = true;
+                        break;
+                }
+                break;
+            case 'I':
+                switch (instr.R.opcode)
+                {
+                    case 0b101011: //SW
+                        MEMORY[(REGISTERS[instr.I.regs] + instr.I.imm) / 4] = REGISTERS[instr.I.regt];
+                        rwMem = true;
+                        break; 
 
-        // PC++
-        PC++;
+                    case 0b100011: //LW
+                        REGISTERS[instr.I.regt] = MEMORY[(REGISTERS[instr.I.regs] + instr.I.imm) / 4];
+                        wbReg = true;
+                        rwMem = true;
+                        break;
+
+                    case 0b001000: //ADDI
+                        REGISTERS[instr.I.regt] = REGISTERS[instr.I.regs] + REGISTERS[instr.I.imm];
+                        wbReg = true;
+                        break;
+
+                    case 0b000100: //BEQ
+                        if (REGISTERS[instr.I.regs] == REGISTERS[instr.I.regt]) {
+                            PC += instr.I.imm;
+                        }
+                        break;
+
+                    case 0b000101: //BNE
+                        if (REGISTERS[instr.I.regs] != REGISTERS[instr.I.regt]) {
+                            PC += instr.I.imm;
+                        }
+                        break;
+                    
+                }
+            
+                break;
+            case 'J':
+                switch (instr.J.opcode) {
+                    case 0b000010: // J
+                        PC += instr.J.imm;
+                        break;
+
+                    case 0b000011: // JAL
+                        REGISTERS[30] = PC;
+                        PC += instr.J.imm;
+                        break;
+                }
+                break;
+        }
+
+        // MEM/WB
+        if (rwMem) {
+            fprintf(fout, "C#%d I%d-MEM\n", clock++, ic);
+        }
+        if (wbReg) {
+            fprintf(fout, "C#%d I%d-WB\n", clock++, ic);
+        }
+
+        ic++;
     }
+
+    file_dumpRegMem(fout);
+    fclose(fout);
 }
 
 int main(int argc, char const *argv[])
 {
+    /* set initial values */
+    REGISTERS[0] = 0;
+
     /* Ask for user input */
     char* fni = (char *)malloc(sizeof(char) * 32);
     char* fno = (char *)malloc(sizeof(char) * 32);
@@ -141,15 +245,16 @@ int main(int argc, char const *argv[])
     printf("Input File: ");
     scanf("%s", fni);
 
-    //printf("Output File: ");
-    //scanf("%s", fno);
+    printf("Output File: ");
+    scanf("%s", fno);
 
     printf("Load Program\n");
     file_load(fni);
-    printf("CPU Run\n");
-    cpu();
 
-    // printf("");
+    printf("CPU Run\n");
+    cpu(fno);
+    
+    printf("Done\n");
 
     return 0;
 }
